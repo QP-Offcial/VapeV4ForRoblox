cb1abb47b06e68a52af0e29b3ec741c86a3458e2
크시 <168262498+xsinew@users.noreply.github.com>
2025-04-28 00:53:04 +0900

    Update

--- main.lua
+++ main.lua
@@ -2,7 +2,9 @@
 
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
-pcall(setclipboard, "discord.gg/qpvxpe")
+if not shared.VapeDeveloper then
+	pcall(setclipboard, "discord.gg/qpvxpe")
+end
 -- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
 	if table.find({'Argon', 'Wave', 'Hyerin'}, ({identifyexecutor()})[1]) then
